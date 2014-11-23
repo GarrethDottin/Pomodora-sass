5b7c81e886706a0102b6caa35f21cabb9ef01aff
Garreth Dottin <garrethdottin@Garreths-MacBook-Air.local>
2014-11-23 01:19:20 -0500

    allow heroku auto assets precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ PomodoraSass::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.1'
 
 
 
