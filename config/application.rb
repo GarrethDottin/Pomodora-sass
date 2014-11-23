977f68778a65961948ea56ecf3a97db23b39f78c
Garreth Dottin <garrethdottin@Garreths-MacBook-Air.local>
2014-11-22 23:56:07 -0500

    fixed sizing issues'

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'sprocket/railtie'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,6 +15,7 @@ module PomodoraSass
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
     config.active_record.whitelist_attributes = false
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Rxun "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
