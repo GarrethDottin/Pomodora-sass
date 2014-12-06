c44eb62aea7e249bbb47449c2ff4c6aca585badf
Garreth Dottin <garrethdottin@Garreths-MacBook-Air.local>
2014-12-06 02:00:10 -0500

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ PomodoraSass::Application.configure do
   # config.assets.css_compressor = :sass
   # Do not fallback to assets pipeline if a precompiled asset is missed.
 
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.compress = true
 
 
